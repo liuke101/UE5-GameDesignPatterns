aea00400af02aaa97e1374427bbd537b628b9227
liuke101 <226256089@qq.com>
2024-07-03 17:26:02 +0800

    build project

---
+++ Source/GameDesignPatterns/GameDesignPatterns.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameDesignPatterns.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameDesignPatterns, "GameDesignPatterns" );
+
+DEFINE_LOG_CATEGORY(LogGameDesignPatterns)
+ 
---
+++ Source/GameDesignPatterns/GameDesignPatternsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameDesignPatternsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGameDesignPatternsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGameDesignPatternsGameMode();
+};
+
+
+
